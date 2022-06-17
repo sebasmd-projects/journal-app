441d4be9525a6e92e7e0c3e4c573e95e67241f86
sebasmd <sebasmoralesd@gmail.com>
2022-06-17 00:17:17 -0500

    Rename app auth a authentication, nueva estrucura de apps

--- Backend/apps/journal/daily_journal/apps.py
+++ Backend/apps/journal/daily_journal/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JournalConfig(AppConfig):
+class DailyJournalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'journal'
+    name = 'daily_journal'
