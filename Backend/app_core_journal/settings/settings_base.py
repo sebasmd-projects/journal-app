31c4cb22cc81987e1603a0b36b956c4205587df6
sebasmd <sebasmoralesd@gmail.com>
2022-06-16 23:24:30 -0500

    Añadido el sensitive_data.json al git ignore

--- Backend/app_core_journal/settings/settings_base.py
+++ Backend/app_core_journal/settings/settings_base.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = BASE_DIR.child('static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.child('public','static')]
 
-MEDIA_URL = BASE_DIR.child('media')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('public','media')
 
 # Default primary key field type
